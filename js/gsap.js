8c3de7e531d45e5cc244907a1117a9155f08e3fb
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-03-26 12:30:36 -0500

    mobile hero view
    responsive view for the hero section was done, also changes to the navbar was made regarding the color

---
+++ js/gsap.js
@@ -0,0 +1,16 @@
+gsap.fromTo('.title', {
+    x: -400
+}, {
+    x: 0,
+    duration: 1,
+    ease: 'power1.out'
+})
+
+// let close = document.querySelector('.close');
+// const rotate = ()=>{
+
+//     gsap.to(close, {
+//         rotation: 360,
+//         duration: 5
+//     })
+// }
