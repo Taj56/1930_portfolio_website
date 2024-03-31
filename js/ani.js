7804b1c899b508a901d6c618e9c9ea8256777237
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-03-31 09:47:29 -0500

    introduction section
    full responsive introduction section was added except for large desktop screens

---
+++ js/ani.js
@@ -0,0 +1,10 @@
+// anime({
+//     targets: '.title',
+//     color: '#000',
+//     translateX: [-400, 0],
+//     translateZ: -99,
+//     duration: 3000
+// })
+
+// const title = document.querySelector('.title');
+
--- js/gsap.js
+++ js/gsap.js
@@ -1,16 +1,7 @@
-gsap.fromTo('.title', {
-    x: -400
-}, {
-    x: 0,
-    duration: 1,
-    ease: 'power1.out'
-})
-
-// let close = document.querySelector('.close');
-// const rotate = ()=>{
-
-//     gsap.to(close, {
-//         rotation: 360,
-//         duration: 5
-//     })
-// }
+// gsap.fromTo('.title', {
+//     x: -400
+// }, {
+//     x: 0,
+//     duration: 1,
+//     ease: 'power1.out'
+// })
